0a095cf3cdcf898054e9a5e3120e7b4fe17141d9
Rizky Maolana  Firdaus <rizkymaulanafrds@gmail.com>
2023-05-22 04:09:21 +0700

    update file

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,15 @@ const express = require("express");
 const products = require("./product.js");
 const Components = require("./component.js");
 const Supplier = require("./supplier.js");
-
 const router = express.Router();
+
+router.get("/", (req, res) => {
+  return res.status(200).json({
+      status: true,
+      message: "welcome to auth api!",
+      data: null
+  })
+})
 
 router.use(products);
 router.use(Components);
